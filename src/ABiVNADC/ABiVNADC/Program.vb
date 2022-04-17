239493463c14bbf2ee5161122e56f7ec5f8aab8c
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-17 13:41:57 -0500

    a very simple boilerplate

---
+++ src/ABiVNADC/ABiVNADC/Program.vb
@@ -0,0 +1,30 @@
+Imports System.IO
+Imports Discord.WebSocket
+Imports Spectre.Console
+
+Module Program
+    Private Const DISCORD_TOKEN_ENVVAR = "DISCORD_TOKEN"
+    Sub Main(args As String())
+        MainAsync.Wait()
+    End Sub
+    Async Function MainAsync() As Task
+        Dim client As New DiscordSocketClient
+        AddHandler client.Log, AddressOf Log
+        AddHandler client.MessageReceived, AddressOf MessageReceived
+        Dim token = System.Environment.GetEnvironmentVariable(DISCORD_TOKEN_ENVVAR)
+
+        Await client.LoginAsync(TokenType.Bot, token)
+        Await client.StartAsync()
+
+        Await Task.Delay(-1)
+    End Function
+
+    Private Function MessageReceived(arg As SocketMessage) As Task
+        Return Task.CompletedTask
+    End Function
+
+    Private Function Log(msg As LogMessage) As Task
+        AnsiConsole.MarkupLine($"Log: [red]{msg}[/]")
+        Return Task.CompletedTask
+    End Function
+End Module
