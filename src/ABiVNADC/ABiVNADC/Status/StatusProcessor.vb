27f3347af2df3bd18b1131abca3671d3d93dc5c9
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-21 14:19:26 -0500

    draw the room

--- src/ABiVNADC/ABiVNADC/Status/StatusProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Status/StatusProcessor.vb
@@ -1,4 +1,6 @@
 ﻿Module StatusProcessor
+    Const OutputColumns = 56
+    Const OutputRows = 28
     Friend Function Run(player As Player, tokens As IEnumerable(Of String)) As String
         If tokens.Any Then
             Return "Round here, we only respond to a raw `status` commmand!"
@@ -10,6 +12,12 @@
                 builder.AppendLine("You don't have a currently selected character!")
             Else
                 builder.AppendLine($"Currently selected character: {character.Name}")
+                Dim canvas As New TextCanvas(OutputColumns, OutputRows, "@"c)
+                canvas.Render(0, 0, LeftDoor)
+                canvas.Render(14, 0, AheadDoor)
+                canvas.Render(42, 0, RightDoor)
+                builder.AppendLine($"```
+{canvas.Output}```")
             End If
             Return builder.ToString
         End If
---
+++ src/ABiVNADC/ABiVNADC/TextCanvas.vb
@@ -0,0 +1,54 @@
+﻿Public Class TextCanvas
+    ReadOnly Property Columns As Long
+    ReadOnly Property Rows As Long
+    Private characters As New List(Of Char)
+    Sub New(lines As IEnumerable(Of String))
+        Rows = lines.Count
+        Columns = lines.First.Length
+        For row = 0 To Rows - 1
+            For column = 0 To Columns - 1
+                characters.Add(lines.Skip(CInt(row)).First()(CInt(column)))
+            Next
+        Next
+    End Sub
+    Sub New(columns As Long, rows As Long, character As Char)
+        Me.Columns = columns
+        Me.Rows = rows
+        While characters.Count < columns * rows
+            characters.Add(character)
+        End While
+    End Sub
+    Function Output() As String
+        Dim builder As New StringBuilder
+        Dim counter As Integer = 0
+        For Each character In characters
+            builder.Append(character)
+            counter += 1
+            If counter Mod Columns = 0 Then
+                builder.AppendLine()
+            End If
+        Next
+        Return builder.ToString
+    End Function
+
+    Friend Sub Render(destinationColumn As Integer, destinationRow As Integer, source As TextCanvas)
+        For column = 0 To source.Columns - 1
+            For row = 0 To source.Rows - 1
+                SetCell(destinationColumn + column, destinationRow + row, source.GetCell(column, row))
+            Next
+        Next
+    End Sub
+
+    Private Sub SetCell(column As Long, row As Long, character As Char?)
+        If character.HasValue AndAlso column >= 0 AndAlso column < Columns AndAlso row >= 0 AndAlso row < Rows Then
+            characters(CInt(column + row * Columns)) = character.Value
+        End If
+    End Sub
+
+    Private Function GetCell(column As Long, row As Long) As Char?
+        If column >= 0 AndAlso column < Columns AndAlso row >= 0 AndAlso row < Rows Then
+            Return characters(CInt(column + row * Columns))
+        End If
+        Return Nothing
+    End Function
+End Class
