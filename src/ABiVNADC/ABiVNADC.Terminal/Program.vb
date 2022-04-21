4e2aa28cb5a968d17b2b2fafaaa8a9004c456e45
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-21 04:39:07 -0500

    i have a terminal running version

---
+++ src/ABiVNADC/ABiVNADC.Terminal/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+
+Module Program
+    Const DATABASE_FILE_NAME = "local.db"
+    Sub Main(args As String())
+        Store.Load(DATABASE_FILE_NAME)
+        Dim player As New Player(AnsiConsole.Ask(Of Long)("[olive]Player Id[/]"))
+        Dim done = False
+        While Not done
+            Dim command = AnsiConsole.Ask(Of String)(">")
+            Select Case command
+                Case "!quit"
+                    done = True
+                Case Else
+                    AnsiConsole.WriteLine()
+                    AnsiConsole.WriteLine(MainProcessor.Run(player, command))
+                    AnsiConsole.WriteLine()
+            End Select
+        End While
+        Store.Save(DATABASE_FILE_NAME)
+    End Sub
+End Module
