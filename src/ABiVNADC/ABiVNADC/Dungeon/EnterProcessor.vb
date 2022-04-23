07e1b5235a7d7270e488daab34d829ff0174bfb1
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-23 09:43:53 -0500

    cannot enter once a dungeon has been entered.

--- src/ABiVNADC/ABiVNADC/Dungeon/EnterProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Dungeon/EnterProcessor.vb
@@ -2,13 +2,17 @@
     Friend Function Run(player As Player, tokens As IEnumerable(Of String)) As String
         Dim character = player.Character
         If character IsNot Nothing Then
-            Dim dungeonName = String.Join(" "c, tokens)
-            Dim dungeon = player.Dungeons.SingleOrDefault(Function(x) x.Name = dungeonName)
-            If dungeon IsNot Nothing Then
-                character.Location = dungeon.StartingLocation
-                Return $"{character.Name} enters {dungeonName}."
+            If character.Location Is Nothing Then
+                Dim dungeonName = String.Join(" "c, tokens)
+                Dim dungeon = player.Dungeons.SingleOrDefault(Function(x) x.Name = dungeonName)
+                If dungeon IsNot Nothing Then
+                    character.Location = dungeon.StartingLocation
+                    Return $"{character.Name} enters {dungeonName}."
+                End If
+                Return $"{dungeonName} doesn't exist!"
+            Else
+                Return $"{character.Name} is already in a dungeon."
             End If
-            Return $"{dungeonName} doesn't exist!"
         End If
         Return $"You don't have a currently selected character. You ain't enterin' NOTHING."
     End Function
