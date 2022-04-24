41b34cea33e7c9f4341a7bfe6b62f2f0244931b9
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-24 05:52:43 -0500

    picking the correct characters for stuff now

---
+++ src/ABiVNADC/ABiVNADC/Images/CharacterImages.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module CharacterImages
+    Friend ReadOnly Goblin As New TextCanvas(Enhance(New List(Of String) From
+                                            {
+                                                "............",
+                                                "............",
+                                                "............",
+                                                "............",
+                                                ".....###....",
+                                                "..... # #...",
+                                                ".....###....",
+                                                ".........#..",
+                                                ".....#.##.#.",
+                                                "....#..##.#.",
+                                                "......#.#...",
+                                                ".....##..#.."
+                                            }))
+    Private Function Enhance(lines As List(Of String)) As List(Of String)
+        Dim result As New List(Of String)
+        For Each line In lines
+            Dim enhancedLine = EnhanceLine(line)
+            result.Add(enhancedLine)
+            result.Add(enhancedLine)
+        Next
+        Return result
+    End Function
+
+    Private Function EnhanceLine(line As String) As String
+        Dim builder As New StringBuilder
+        For Each character In line
+            builder.Append(character)
+            builder.Append(character)
+        Next
+        Return builder.ToString
+    End Function
+    <Extension>
+    Function Image(characterType As CharacterType) As TextCanvas
+        Select Case characterType
+            Case CharacterType.Goblin
+                Return Goblin
+            Case Else
+                Throw New NotImplementedException
+        End Select
+    End Function
+End Module
--- src/ABiVNADC/ABiVNADC/Status/StatusProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Status/StatusProcessor.vb
@@ -1,6 +1,4 @@
 ﻿Module StatusProcessor
-    Const OutputColumns = 56
-    Const OutputRows = 28
     Friend Function Run(player As Player, tokens As IEnumerable(Of String)) As String
         If tokens.Any Then
             Return "Round here, we only respond to a raw `status` commmand!"
@@ -29,33 +27,5 @@
             End If
         End If
         Return builder.ToString
-    End Function
-
-    Friend Function DrawPOV(player As Player) As TextCanvas
-        Dim direction = player.AheadDirection.Value
-        Dim leftDirection = direction.LeftDirection
-        Dim rightDirection = direction.RightDirection
-        Dim canvas As New TextCanvas(OutputColumns, OutputRows, "@"c)
-        Dim location = player.Character.Location
-        Dim routes = location.Routes
-        If routes.ContainsKey(leftDirection) Then
-            canvas.Render(0, 0, LeftDoor)
-        Else
-            canvas.Render(0, 0, LeftWall)
-        End If
-        If routes.ContainsKey(direction) Then
-            canvas.Render(14, 0, AheadDoor)
-        Else
-            canvas.Render(14, 0, AheadWall)
-        End If
-        If routes.ContainsKey(rightDirection) Then
-            canvas.Render(42, 0, RightDoor)
-        Else
-            canvas.Render(42, 0, RightWall)
-        End If
-        If Not location.Inventory.IsEmpty Then
-            canvas.Render(22, 13, Chest, "."c)
-        End If
-        Return canvas
     End Function
 End Module
