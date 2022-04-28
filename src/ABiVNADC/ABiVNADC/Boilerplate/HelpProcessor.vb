58d08c00d04f3f9f59f37d1b9d64cc70a00b586b
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-04-28 04:03:34 -0500

    no more topical help, just send them to the wiki

--- src/ABiVNADC/ABiVNADC/Boilerplate/HelpProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Boilerplate/HelpProcessor.vb
@@ -1,26 +1,6 @@
 ﻿Module HelpProcessor
     Friend Function Run(player As Player, tokens As IEnumerable(Of String)) As String
-        If tokens.Any Then
-            Return TopicalHelp(String.Join(" "c, tokens))
-        Else
-            Return "Check out the GitHub Wiki!
+        Return "Check out the GitHub Wiki!
 https://github.com/playdeezgames/DungeonCrawlingDiscordBot/wiki"
-        End If
-    End Function
-
-    Private Function TopicalHelp(topic As String) As String
-        Select Case topic
-            Case $"{CreateText}"
-                Return $"Create Command:
-- create character (name) : creates a character with that name. no duplicates
-- create dungeon (difficulty) (name) : creates a dungeon with that name. no duplicates
-    difficulty levels: yermom, easy, normal, difficult, too"
-            Case $"{DeleteText}"
-                Return $"Create Command:
-- delete character (name) : creates a character with that name. no duplicates
-- delete dungeon (name) : creates a dungeon with that name. no duplicates"
-            Case Else
-                Return "I don't know how to help that."
-        End Select
     End Function
 End Module
