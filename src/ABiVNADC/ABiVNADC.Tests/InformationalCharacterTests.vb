897213b6ae5b43f875babced532f1576fe4295d1
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-30 05:11:24 -0500

    informational character tests

---
+++ src/ABiVNADC/ABiVNADC.Tests/InformationalCharacterTests.vb
@@ -0,0 +1,13 @@
+﻿<Collection("One Big Collection")>
+Public Class InformationalCharacterTests
+    Public Sub New()
+        Store.Reset()
+        MainProcessor.Run(DummyPlayer, "create character test")
+    End Sub
+    <Theory>
+    <InlineData("inventory")>
+    <InlineData("status")>
+    Public Sub ShouldReturnInformationAndNotExplode(command As String)
+        MainProcessor.Run(DummyPlayer, command).ShouldNotBeEmpty
+    End Sub
+End Class
