f0799d9d6add3b29831cb8c12309787c07f66ba7
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-30 05:19:00 -0500

    informational in play tests

---
+++ src/ABiVNADC/ABiVNADC.Tests/InformationalInPlayTests.vb
@@ -0,0 +1,16 @@
+﻿<Collection("One Big Collection")>
+Public Class InformationalInPlayTests
+    Public Sub New()
+        Store.Reset()
+        MainProcessor.Run(DummyPlayer, "create character test")
+        MainProcessor.Run(DummyPlayer, "create dungeon yermom test")
+        MainProcessor.Run(DummyPlayer, "enter test")
+    End Sub
+    <Theory>
+    <InlineData("enemies")>
+    <InlineData("ground")>
+    <InlineData("look")>
+    Public Sub ShouldReturnInformationAndNotExplode(command As String)
+        MainProcessor.Run(DummyPlayer, command).ShouldNotBeEmpty
+    End Sub
+End Class
