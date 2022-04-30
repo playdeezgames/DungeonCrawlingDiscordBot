7e04ebcdcdd97df989fb6be811381f674352017c
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-30 05:26:33 -0500

    navigation tests

---
+++ src/ABiVNADC/ABiVNADC.Tests/NavigationInPlayTests.vb
@@ -0,0 +1,20 @@
+﻿<Collection("One Big Collection")>
+Public Class NavigationInPlayTests
+    Public Sub New()
+        Store.Reset()
+        MainProcessor.Run(DummyPlayer, "create character test")
+        MainProcessor.Run(DummyPlayer, "create dungeon yermom test")
+        MainProcessor.Run(DummyPlayer, "enter test")
+        Dim character = DummyPlayer.Character
+        For Each enemy In DummyPlayer.Character.Location.Enemies(character)
+            enemy.Destroy()
+        Next
+    End Sub
+    <Theory>
+    <InlineData("around")>
+    <InlineData("left")>
+    <InlineData("right")>
+    Public Sub ShouldAllowNavigation(command As String)
+        MainProcessor.Run(DummyPlayer, command).ShouldNotBeEmpty
+    End Sub
+End Class
