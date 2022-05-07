5fae331a8d96336f05aa03d1ce8d354d7940f441
TheGrumpyGameDev <ernestpazera@msn.com>
2022-05-07 17:48:24 -0500

    ground and inventory processors

--- src/ABiVNADC/ABiVNADC/Inventory/GroundProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Inventory/GroundProcessor.vb
@@ -1,25 +1,27 @@
 ﻿Module GroundProcessor
     Friend Sub Run(player As Player, builder As StringBuilder, tokens As IEnumerable(Of String))
-        If tokens.Any Then
-            builder.AppendLine("Just `ground`, ok?")
-            Return
-        End If
-        Dim character = player.Character
-        If character Is Nothing Then
-            builder.AppendLine("You have no current character!")
-            Return
-        End If
-        Dim location = character.Location
-        If location Is Nothing Then
-            builder.AppendLine("Yer character is not in a dungeon.")
-            Return
-        End If
-        Dim inventory = location.Inventory
-        If inventory.IsEmpty Then
-            builder.AppendLine("There is nothing on the ground.")
-            Return
-        End If
-        Dim itemStacks = inventory.StackedItems
-        builder.AppendLine($"Items on the ground: {String.Join(", ", itemStacks.Select(Function(entry) $"{entry.Key.Name}(x{entry.Value.Count})"))}")
+        RequireNoTokens(
+            tokens,
+            GroundText,
+            builder,
+            Sub()
+                RequireCharacter(
+                    player,
+                    builder,
+                    Sub(character)
+                        RequireLocation(
+                            character,
+                            builder,
+                            Sub(location)
+                                Dim inventory = location.Inventory
+                                If inventory.IsEmpty Then
+                                    builder.AppendLine("There is nothing on the ground.")
+                                    Return
+                                End If
+                                Dim itemStacks = inventory.StackedItems
+                                builder.AppendLine($"Items on the ground: {String.Join(", ", itemStacks.Select(Function(entry) $"{entry.Key.Name}(x{entry.Value.Count})"))}")
+                            End Sub)
+                    End Sub)
+            End Sub)
     End Sub
 End Module
--- src/ABiVNADC/ABiVNADC/Inventory/InventoryProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Inventory/InventoryProcessor.vb
@@ -1,20 +1,22 @@
 ﻿Module InventoryProcessor
     Friend Sub Run(player As Player, builder As StringBuilder, tokens As IEnumerable(Of String))
-        If tokens.Any Then
-            builder.AppendLine("Just `inventory`, ok?")
-            Return
-        End If
-        Dim character = player.Character
-        If character Is Nothing Then
-            builder.AppendLine("You have no current character!")
-            Return
-        End If
-        Dim inventory = character.Inventory
-        If inventory.IsEmpty Then
-            builder.AppendLine($"{character.Name} has nothing in their inventory.")
-            Return
-        End If
-        Dim itemStacks = inventory.StackedItems
-        builder.AppendLine($"{character.Name}'s Inventory: {String.Join(", ", itemStacks.Select(Function(entry) $"{entry.Key.Name}(x{entry.Value.Count})"))}")
+        RequireNoTokens(
+            tokens,
+            InventoryText,
+            builder,
+            Sub()
+                RequireCharacter(
+                    player,
+                    builder,
+                    Sub(character)
+                        Dim inventory = character.Inventory
+                        If inventory.IsEmpty Then
+                            builder.AppendLine($"{character.Name} has nothing in their inventory.")
+                            Return
+                        End If
+                        Dim itemStacks = inventory.StackedItems
+                        builder.AppendLine($"{character.Name}'s Inventory: {String.Join(", ", itemStacks.Select(Function(entry) $"{entry.Key.Name}(x{entry.Value.Count})"))}")
+                    End Sub)
+            End Sub)
     End Sub
 End Module
