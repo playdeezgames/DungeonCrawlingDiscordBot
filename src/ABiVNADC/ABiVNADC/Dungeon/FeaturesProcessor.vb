1e93a5210480f1e12a4ceeb152f11524a042a34f
TheGrumpyGameDev <ernestpazera@msn.com>
2022-05-07 13:09:34 -0500

    drop processor

--- src/ABiVNADC/ABiVNADC/Dungeon/FeaturesProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Dungeon/FeaturesProcessor.vb
@@ -1,23 +1,25 @@
 ﻿Module FeaturesProcessor
     Friend Sub Run(player As Player, builder As StringBuilder, tokens As IEnumerable(Of String))
-        If tokens.Any Then
-            builder.AppendLine("The command is just `features`.")
-            Return
-        End If
-        RequireCharacter(
-            player,
+        RequireNoTokens(
+            tokens,
+            FeaturesText,
             builder,
-            Sub(character)
-                RequireLocation(
-                    character,
+            Sub()
+                RequireCharacter(
+                    player,
                     builder,
-                    Function(location)
-                        Dim features = location.Features
-                        If Not features.Any Then
-                            Return $"There are no features here."
-                        End If
-                        Return $"Features present: {String.Join(", ", features.Select(Function(x) x.Name))}"
-                    End Function)
+                    Sub(character)
+                        RequireLocation(
+                            character,
+                            builder,
+                            Sub(location)
+                                Dim features = location.Features
+                                If Not features.Any Then
+                                    builder.AppendLine($"There are no features here.")
+                                End If
+                                builder.AppendLine($"Features present: {String.Join(", ", features.Select(Function(x) x.Name))}")
+                            End Sub)
+                    End Sub)
             End Sub)
     End Sub
 End Module
--- src/ABiVNADC/ABiVNADC/Inventory/DropProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Inventory/DropProcessor.vb
@@ -1,34 +1,34 @@
 ﻿Module DropProcessor
     Friend Sub Run(player As Player, builder As StringBuilder, tokens As IEnumerable(Of String))
-        If Not tokens.Any Then
-            builder.AppendLine("Drop what?")
-            Return
-        End If
-        Dim itemTypeName = String.Join(" "c, tokens)
+        RequireTokens(
+            tokens,
+            "Drop what?",
+            builder,
+            Sub()
+                RequireItemType(
+                    tokens,
+                    builder,
+                    Sub(itemType)
+                        RequireCharacter(
+                            player,
+                            builder,
+                            Sub(character)
+                                RequireLocation(
+                                    character,
+                                    builder,
+                                    Sub(location)
+                                        Dim itemStacks = character.Inventory.StackedItems
+                                        If Not itemStacks.ContainsKey(itemType) Then
+                                            builder.AppendLine($"{character.Name} doesn't have any `{itemType.Name}`.")
+                                            Return
+                                        End If
+                                        Dim item = itemStacks(itemType).First
+                                        character.Location.Inventory.Add(item)
+                                        builder.AppendLine($"{character.Name} drops {itemType.Name}")
+                                    End Sub)
+                            End Sub)
+                    End Sub)
+            End Sub)
 
-        Dim itemType = ParseItemType(itemTypeName)
-        If itemType = ItemType.None Then
-            builder.AppendLine($"I don't know what a `{itemTypeName}` is.")
-            Return
-        End If
-
-        Dim character = player.Character
-        If character Is Nothing Then
-            builder.AppendLine("You have no current character.")
-            Return
-        End If
-        Dim location = character.Location
-        If location Is Nothing Then
-            builder.AppendLine($"{character.Name} is not in a dungeon.")
-            Return
-        End If
-        Dim itemStacks = character.Inventory.StackedItems
-        If Not itemStacks.ContainsKey(itemType) Then
-            builder.AppendLine($"{character.Name} doesn't have any `{itemTypeName}`.")
-            Return
-        End If
-        Dim item = itemStacks(itemType).First
-        character.Location.Inventory.Add(item)
-        builder.AppendLine($"{character.Name} drops {itemType.Name}")
     End Sub
 End Module
