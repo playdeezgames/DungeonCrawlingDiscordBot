ee519cc089ac586a4111597702f3b79fd5ce7466
TheGrumpyGameDev <ernestpazera@msn.com>
2022-05-07 17:42:40 -0500

    equip processor

--- src/ABiVNADC/ABiVNADC/Inventory/EquipProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Inventory/EquipProcessor.vb
@@ -1,21 +1,22 @@
 ﻿Module EquipProcessor
     Friend Sub Run(player As Player, builder As StringBuilder, tokens As IEnumerable(Of String))
-        If Not tokens.Any Then
-            builder.AppendLine("The command is `equip (item)`.")
-            Return
-        End If
-        RequireCharacter(
-            player,
+        RequireTokens(
+            tokens,
+            $"{EquipText} (item)",
             builder,
-            Sub(character)
-                Dim itemTypeName = String.Join(" "c, tokens)
-                Dim itemType = ParseItemType(itemTypeName)
-                If itemType = ItemType.None Then
-                    builder.AppendLine($"I don't know what a `{itemTypeName}` is.")
-                    Return
-                End If
-                Dim output = character.Equip(itemType)
-                builder.AppendLine(DoCounterAttacks(character, output))
+            Sub()
+                RequireCharacter(
+                    player,
+                    builder,
+                    Sub(character)
+                        RequireItemType(
+                            tokens,
+                            builder,
+                            Sub(itemType)
+                                Dim output = character.Equip(itemType)
+                                builder.AppendLine(DoCounterAttacks(character, output))
+                            End Sub)
+                    End Sub)
             End Sub)
     End Sub
 End Module
