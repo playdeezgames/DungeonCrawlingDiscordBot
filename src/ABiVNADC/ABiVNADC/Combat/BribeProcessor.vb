e284f7e894de602f32a364c5c5cb72317588f652
TheGrumpyGameDev <ernestpazera@msn.com>
2022-05-06 20:45:46 -0500

    require character on bribe processor

---
+++ src/ABiVNADC/ABiVNADC/Combat/BribeProcessor.vb
@@ -0,0 +1,27 @@
+﻿Module BribeProcessor
+    Friend Function Run(player As Player, tokens As IEnumerable(Of String)) As String
+        If Not tokens.Any Then
+            Return "Bribe with what? Empty handed bribes are rude!"
+        End If
+        Dim itemTypeName = StitchTokens(tokens)
+        Dim itemType = ParseItemType(itemTypeName)
+        If itemType = ItemType.None Then
+            Return $"I don't know what a `{itemTypeName}` is."
+        End If
+        Return RequireCharacter(
+            player,
+            Function(character)
+                If Not character.HasLocation Then
+                    Return $"{character.FullName} is not in a dungeon."
+                End If
+                If Not character.Inventory.StackedItems.Keys.Contains(itemType) Then
+                    Return $"{character.FullName} doesn't have any {itemType.Name}."
+                End If
+                Dim enemy As Character = character.Location.Enemies(character).FirstOrDefault(Function(x) x.TakesBribe(itemType))
+                If enemy Is Nothing Then
+                    Return $"No enemy in this location will take that."
+                End If
+                Return DoCounterAttacks(player, character.BribeEnemy(enemy, itemType))
+            End Function)
+    End Function
+End Module
---
+++ src/ABiVNADC/ABiVNADC/Utility.vb
@@ -0,0 +1,8 @@
+﻿Module Utility
+    Function RequireCharacter(player As Player, handler As Func(Of Character, String)) As String
+        If Not player.HasCharacter Then
+            Return "You have no current character."
+        End If
+        Return handler(player.Character)
+    End Function
+End Module
