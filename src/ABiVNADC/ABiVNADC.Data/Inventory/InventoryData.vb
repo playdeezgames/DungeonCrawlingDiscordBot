5c626971f8a334fcf6e483acbb048e21fc23dd9a
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-05-12 04:55:00 -0500

    inventory orphan fix

--- src/ABiVNADC/ABiVNADC.Data/Inventory/InventoryData.vb
+++ src/ABiVNADC/ABiVNADC.Data/Inventory/InventoryData.vb
@@ -13,15 +13,20 @@
 
     Public Sub ClearOrphans()
         Initialize()
-        ExecuteNonQuery(
-            $"DELETE i 
-            FROM [{TableName}] i 
-            LEFT JOIN [{CharacterInventoryData.TableName}] c 
-            ON i.[{InventoryIdColumn}]=c.[{CharacterInventoryData.InventoryIdColumn}] 
-            LEFT JOIN [{LocationInventoryData.TableName}] l 
-            ON i.[{InventoryIdColumn}]=l.[{LocationInventoryData.InventoryIdColumn}] 
-            WHERE 
-                c.[{CharacterInventoryData.InventoryIdColumn}] IS NULL AND 
-                l.[{LocationInventoryData.InventoryIdColumn}] IS NULL;")
+        Dim orphans =
+            ExecuteReader(Of Long)(
+                Function(reader) CLng(reader(InventoryIdColumn)),
+                $"SELECT i.[{InventoryIdColumn}]
+                FROM [{TableName}] i 
+                LEFT JOIN [{CharacterInventoryData.TableName}] c 
+                ON i.[{InventoryIdColumn}]=c.[{CharacterInventoryData.InventoryIdColumn}] 
+                LEFT JOIN [{LocationInventoryData.TableName}] l 
+                ON i.[{InventoryIdColumn}]=l.[{LocationInventoryData.InventoryIdColumn}] 
+                WHERE 
+                    c.[{CharacterInventoryData.InventoryIdColumn}] IS NULL AND 
+                    l.[{LocationInventoryData.InventoryIdColumn}] IS NULL;")
+        For Each orphan In orphans
+            ExecuteNonQuery($"DELETE FROM [{TableName}] WHERE [{InventoryIdColumn}]=@{InventoryIdColumn};", MakeParameter($"@{InventoryIdColumn}", orphan))
+        Next
     End Sub
 End Module
