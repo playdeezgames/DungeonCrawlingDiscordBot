5da7e2873ea92d6d2cf16c0cea7fcae979154605
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-05-18 04:37:03 -0500

    equip slot descriptors

---
+++ src/ABiVNADC/ABiVNADC.Game/Inventory/EquipSlot/EquipSlot.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Enum EquipSlot
+    None
+    Weapon
+    Shield
+    Head
+    Body
+    Legs
+    Back
+End Enum
+Public Module EquipSlotExtensions
+    Public Function AllEquipSlots() As IEnumerable(Of EquipSlot)
+        Return EquipSlotDescriptors.Keys
+    End Function
+    <Extension>
+    Function Name(equipSlot As EquipSlot) As String
+        Return EquipSlotDescriptors(equipSlot).Name
+    End Function
+
+    Public Function ParseEquipSlot(equipSlotName As String) As EquipSlot
+        Return AllEquipSlots.SingleOrDefault(Function(x) x.Name = equipSlotName)
+    End Function
+End Module
---
+++ src/ABiVNADC/ABiVNADC.Game/Inventory/EquipSlot/EquipSlotDescriptor.vb
@@ -0,0 +1,51 @@
+﻿Public Class EquipSlotDescriptor
+    Property Name As String
+
+End Class
+Module EquipSlotDescriptorExtensions
+    Friend ReadOnly EquipSlotDescriptors As New Dictionary(Of EquipSlot, EquipSlotDescriptor) From
+        {
+            {
+                EquipSlot.Back,
+                New EquipSlotDescriptor With
+                {
+                    .Name = "back"
+                }
+            },
+            {
+                EquipSlot.Body,
+                New EquipSlotDescriptor With
+                {
+                    .Name = "body"
+                }
+            },
+            {
+                EquipSlot.Head,
+                New EquipSlotDescriptor With
+                {
+                    .Name = "head"
+                }
+            },
+            {
+                EquipSlot.Legs,
+                New EquipSlotDescriptor With
+                {
+                    .Name = "legs"
+                }
+            },
+            {
+                EquipSlot.Shield,
+                New EquipSlotDescriptor With
+                {
+                    .Name = "shield"
+                }
+            },
+            {
+                EquipSlot.Weapon,
+                New EquipSlotDescriptor With
+                {
+                    .Name = "weapon"
+                }
+            }
+        }
+End Module
