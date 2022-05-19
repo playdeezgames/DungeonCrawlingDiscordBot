ae9f9d82932db319ed273ce8e5cdea5efb98e868
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-05-19 15:28:57 -0500

    bribe

--- src/ABiVNADC/ABiVNADC/Combat/BribeProcessor.vb
+++ src/ABiVNADC/ABiVNADC/Combat/BribeProcessor.vb
@@ -5,26 +5,21 @@
             "Bribe with what? Empty handed bribes are rude!",
             builder,
             Sub()
-                RequireItemType(
-                    tokens,
+                RequireCharacterLocation(
+                    player,
                     builder,
-                    Sub(itemType)
-                        RequireCharacterLocation(
-                            player,
+                    Sub(character, location)
+                        RequireItemName(
+                            tokens,
+                            AddressOf character.Inventory.FindItemsByName,
                             builder,
-                            Sub(character, location)
-                                RequireItemName(
-                                    tokens,
-                                    AddressOf character.Inventory.FindItemsByName,
-                                    builder,
-                                    Sub(item)
-                                        Dim enemy As Character = character.Location.Enemies(character).FirstOrDefault(Function(x) x.TakesBribe(item))
-                                        If enemy Is Nothing Then
-                                            builder.AppendLine($"No enemy in this location will take that.")
-                                            Return
-                                        End If
-                                        player.BribeEnemy(enemy, item, builder)
-                                    End Sub)
+                            Sub(item)
+                                Dim enemy As Character = character.Location.Enemies(character).FirstOrDefault(Function(x) x.TakesBribe(item))
+                                If enemy Is Nothing Then
+                                    builder.AppendLine($"No enemy in this location will take that.")
+                                    Return
+                                End If
+                                player.BribeEnemy(enemy, item, builder)
                             End Sub)
                     End Sub)
             End Sub)
