b441d6311128461487b086983c106352fe459c71
TheGrumpyGameDev <ernestpazera@msn.com>
2022-05-28 04:37:11 -0500

    dungeon location data

--- src/ABiVNADC/ABiVNADC.Data/Dungeon/DungeonLocationData.vb
+++ src/ABiVNADC/ABiVNADC.Data/Dungeon/DungeonLocationData.vb
@@ -20,11 +20,7 @@
     End Function
 
     Public Sub Write(dungeonId As Long, locationId As Long)
-        Initialize()
-        ExecuteNonQuery(
-            $"REPLACE INTO [{TableName}]([{DungeonIdColumn}],[{LocationIdColumn}]) VALUES(@{DungeonIdColumn},@{LocationIdColumn});",
-            MakeParameter($"@{DungeonIdColumn}", dungeonId),
-            MakeParameter($"@{LocationIdColumn}", locationId))
+        ReplaceRecord(AddressOf Initialize, TableName, DungeonIdColumn, dungeonId, LocationIdColumn, locationId)
     End Sub
 
     Friend Sub ClearForDungeon(dungeonId As Long)
